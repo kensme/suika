c798cfa9325de39c930a00826d6b7c4e8f5b86a2
kensme <kenste.sam@gmail.com>
2023-11-13 17:20:38 +1300

    add action for ghpages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
-import { defineConfig } from 'astro/config';
-import wasm from 'vite-plugin-wasm';
+import { defineConfig } from "astro/config";
+import wasm from "vite-plugin-wasm";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://kensme.github.io/suika/",
+  base: "/suika",
   integrations: [tailwind()],
   vite: {
-    plugins: [wasm()]
-  }
+    plugins: [wasm()],
+  },
 });

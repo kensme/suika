fb66fc0f9e1b0e099a6d4cebf75314bf3c9ef498
kensme <kenste.sam@gmail.com>
2023-11-15 20:14:03 +1300

    change site

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,12 @@ import wasm from "vite-plugin-wasm";
 
 import tailwind from "@astrojs/tailwind";
 
+const base = process.env.NODE_ENV === "production" ? "/suika/" : "/";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://kensme.github.io/",
-  base: "/",
+  site: "https://kensme.github.io/suika/",
+  base: base,
   integrations: [tailwind()],
   vite: {
     plugins: [wasm()],
